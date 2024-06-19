dcd47d7e2672dd473a86062d60667a88a96b822c
peanut-lover <jihoson94@gmail.com>
2024-06-19 16:46:05 +0900

    Time Zone Tests

--- Timezone/Program.cs
+++ Timezone/Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var utcNowDate = DateTime.UtcNow;
-Console.WriteLine(utcNowDate.Kind);
-Console.WriteLine(utcNowDate);
 var localNowDate = DateTime.Now;
-Console.WriteLine(localNowDate.Kind);
-Console.WriteLine(localNowDate);
+Console.WriteLine(localNowDate.ToString("yyyy-MM-dd HH:mm:ss"));
+Console.WriteLine(localNowDate.ToString());
+Console.WriteLine(localNowDate.ToBinary());
+Console.WriteLine(localNowDate.ToLongDateString());
+Console.WriteLine(localNowDate.ToLongTimeString());
+Console.WriteLine(localNowDate.ToShortDateString());
+Console.WriteLine(localNowDate.ToShortTimeString());
+
+Console.WriteLine(DateTime.MinValue.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ssZ"));
